f5578d8065355b550995d8a5c7b64d44eb5988c6
Vasiliy Timoshenko <v_timoshenko548796@mail.ru>
2024-05-21 16:46:28 +0300

    4 адблокера

--- backend/neuro/nn.py
+++ backend/neuro/nn.py
@@ -36,7 +36,7 @@ def saveUrlToImage(url, resultImageName):
                   "https://chromewebstore.google.com/detail/adguard-антибаннер/bgnkhhnnamicmpeenaelnjfhikgbkllg",
                   # "https://chromewebstore.google.com/detail/adblocker-stands/lgblnfidahcdcjddiepkckcfdhpknnjh",
                   # "https://chromewebstore.google.com/detail/adblock-%E2%80%94-best-ad-blocker/gighmmpiobklfepjocnamgkkbiglidom",
-                  # "https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm",
+                  "https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm",
                   "https://chromewebstore.google.com/detail/stop-reclame/ohmkcnojelglgphmkgmofjlmpoelccjh"]
     options.add_argument('--load-extension=' + ','.join(
         [Extension(adblocker).load(with_command_line_option=False) for adblocker in adblockers]))
